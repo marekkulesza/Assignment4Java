2ad47eacfd428bd6a01c1653c32499185d0af21a
marekkulesza <66383506+marekkulesza@users.noreply.github.com>
2021-02-28 12:41:34 -0500

    moved the psvm to Main.java

--- src/Assignment4/Main.java
+++ src/Assignment4/Main.java
@@ -1,7 +1,18 @@
 package Assignment4;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("this should be the main world");
+        SlotMachine Yeet1 = new SlotMachine(new String[]{"7", "Cherries", "Bar", "King", "Bob"},4);
+        Yeet1.rollers();
+        System.out.println(Yeet1);
+        Yeet1.rollers();
+        System.out.println(Yeet1);
+        Yeet1.rollers();
+        System.out.println(Yeet1);
+        Yeet1.rollers();
+        System.out.println(Yeet1);
+        Yeet1.rollers();
+        System.out.println(Yeet1);
     }
 }
--- src/Assignment4/SlotMachine.java
+++ src/Assignment4/SlotMachine.java
@@ -52,17 +52,5 @@ public class SlotMachine {
         return "SlotMachine " + wheelList + " You got " + (innerCounter) + " Point";
     }
 
-    public static void main(String[] args) {
-        SlotMachine Yeet1 = new SlotMachine(new String[]{"7", "Cherries", "Bar", "King", "Bob"},4);
-        Yeet1.rollers();
-        System.out.println(Yeet1);
-        Yeet1.rollers();
-        System.out.println(Yeet1);
-        Yeet1.rollers();
-        System.out.println(Yeet1);
-        Yeet1.rollers();
-        System.out.println(Yeet1);
-        Yeet1.rollers();
-        System.out.println(Yeet1);
-    }
+
 }
