1de096aed38c34f25293a16a3c60ca16296a42ff
marekkulesza <66383506+marekkulesza@users.noreply.github.com>
2021-02-27 21:07:54 -0500

    Added onto the SlotMachine a lot of things, gave it a Wheel[] lists not working as of now

--- src/Assignment4/SlotMachine.java
+++ src/Assignment4/SlotMachine.java
@@ -1,13 +1,43 @@
 package Assignment4;
 
+import java.util.Arrays;
+
 public class SlotMachine {
 
-    private String[] x;
 
-    public SlotMachine(String[] x) {
-        this.x = new String[x.length];
+    private final String[] facesList;
+    private final int numWheels;
+    private final String[] colours = {"BLUE", "RED", "GREEN", "ORANGE", "PINK", "GRAY", "MAGENTA"};
+    private Wheel[] wheelList;
+
+    @Override
+    public String toString() {
+        return "SlotMachine{" +
+                "facesList=" + Arrays.toString(facesList) +
+                ", numWheels=" + numWheels +
+                ", wheelList=" + Arrays.toString(wheelList) +
+                '}';
+    }
+
+    public SlotMachine(String[] x, int numWheels) {
+
+        this.facesList = new String[x.length];
+        this.numWheels = numWheels;
+        
         for (int i = 0; i < x.length; i++) {
-            this.x[i] = x[i];
+            this.facesList[i] = x[i];
+        }
+
+        for (int i = 0; i < numWheels; i++){
+            wheelList[i] = new Wheel(facesList);
         }
     }
+
+    public static void main(String[] args) {
+        SlotMachine Yeet1 = new SlotMachine(new String[]{"7", "Cherries", "Bar", "King", "Bob"},4);
+
+        System.out.println(Yeet1);
+
+    }
+
 }
